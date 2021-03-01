6e171a984650c41e2ae87f405d66760c88bbe501
djmelok <djmelok@mail.ru>
2021-03-01 04:11:02 +0300

    add dist

--- layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-nuxt
-</template>
-
-<style lang="scss">
-body {
-  background-color: #141414;
-}
-</style>
--- plugins/VueMask.js
+++
@@ -1,7 +0,0 @@
-// Добавляет возможность форматирования ввода используя маски
-// https://github.com/probil/v-mask
-
-import Vue from 'vue';
-import VueMask from 'v-mask';
-
-Vue.use(VueMask);
